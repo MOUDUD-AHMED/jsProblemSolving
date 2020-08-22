eae5945e9723b36813a81cb9d5b86853a34a0d64
MOUDUD-AHMED <ahmed.moududcd@gmail.com>
2020-08-22 19:29:11 +0600

    convert inch to feet.

---
+++ inchTofeet.js
@@ -0,0 +1,13 @@
+function inchTofeet(inch) {
+    var feet = inch/12;
+    return feet;
+}
+
+var input = inchTofeet(144);
+console.log(input);
+var input1 = inchTofeet(168);
+console.log(input1);
+var input2 = inchTofeet(216);
+console.log(input2);
+var input3 = inchTofeet(324);
+console.log(input3);
